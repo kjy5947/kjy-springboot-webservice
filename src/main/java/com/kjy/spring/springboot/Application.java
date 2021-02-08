30243a959097f5f7085ea3212ed83e68d06bd133
kjy <kjy5947@gmail.com>
2021-02-08 22:39:37 +0900

    Adding gitignore

---
+++ src/main/java/com/kjy/spring/springboot/Application.java
@@ -0,0 +1,10 @@
+package com.kjy.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
+
